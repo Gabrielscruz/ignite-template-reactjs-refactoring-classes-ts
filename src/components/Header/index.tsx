e5c9719c0bb02258bb6dde5c6664e268751e8049
Gabrielscruz <54606517+Gabrielscruz@users.noreply.github.com>
2022-02-23 23:19:41 -0300

    rafactoring-Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,11 @@
-import { Component } from 'react';
 import { FiPlusSquare } from 'react-icons/fi';
-
 import { Container } from './styles';
 import Logo from '../../assets/logo.svg';
 
-class Header extends Component {
-  render() {
-    const { openModal } = this.props;
+interface ModalProps  {
+  openModal: () => void;
+}
+export function Header({openModal}:ModalProps){
 
     return (
       <Container>
@@ -29,6 +28,6 @@ class Header extends Component {
       </Container>
     )
   }
-};
+
 
 export default Header;
